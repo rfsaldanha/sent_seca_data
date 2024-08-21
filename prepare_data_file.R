df4c4312b1cd00bb7b6cb5d9189a213c0f66b519
Raphael Saldanha <rfsaldanha@gmail.com>
2024-08-21 14:23:08 +0200

    Prepare data file for application

---
+++ prepare_data_file.R
@@ -0,0 +1,11 @@
+rm(list = ls())
+
+load(file = "dados_sent_seca_original.RData")
+
+rm(tab5, tab4, tab2)
+
+tab5 <- readRDS("tab5.rds")
+tab4 <- readRDS("tab4.rds")
+tab2 <- readRDS("tab2.rds")
+
+save.image("../sent_seca/dados_sent_seca_v2.RData")
